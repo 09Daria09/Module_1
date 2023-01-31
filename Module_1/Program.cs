365f40e25f35a4e408988349731cb6d8b2e74426
Daria <kukuruzadasha86@gmail.com>
2023-02-01 00:49:49 +0200

    add

--- Module_1/Program.cs
+++ Module_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,15 +18,15 @@ namespace Module_1
             {
                 Console.Write("Введите число от 1 до 100: ");
                 int num = Convert.ToInt32(Console.ReadLine());
-                if(num % 2 == 0 && num % 3 == 0)
+                if (num % 2 == 0 && num % 3 == 0)
                 {
                     Console.WriteLine("Fizz Buzz");
                 }
-                else if(num%2 == 0)
+                else if (num % 2 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
-                else if(num%3 == 0)
+                else if (num % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
@@ -43,7 +44,7 @@ namespace Module_1
                 int persent = Convert.ToInt32(Console.ReadLine());
 
                 answer = (num * persent) / 100;
-                Console.WriteLine("Ответ: {0}",answer);
+                Console.WriteLine("Ответ: {0}", answer);
             }
             if (test == "3")
             {
@@ -52,8 +53,53 @@ namespace Module_1
                 Console.WriteLine("Введите четыре числа: ");
                 string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
                 for (int i = 0; i < (size < str.Length ? size : str.Length); ++i)
-                   numbers += str[i];
+                    numbers += str[i];
                 Console.Write("Число: {0}\n", numbers);
+            }
+            if (test == "4")
+            {
+
+                int size = 6;
+                int[] numbers = new int[size];
+                int firstnum = 0;
+                Console.WriteLine("Введите шестизначное число: ");
+                string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < (size < str.Length ? size : str.Length); ++i)
+                    firstnum = Convert.ToInt32(str[i]);
+
+                for (int i = size - 1; i >= 0; i--)
+                {
+                    numbers[i] = firstnum % 10;
+                    firstnum /= 10;
+                }
+
+                if (numbers.Length != 6)
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    return;
+                }
+
+                Console.Write("Введите первый разряд: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите второй разряд: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                int rezerv = numbers[num - 1];
+                numbers[num - 1] = numbers[num1 - 1];
+                numbers[num1 - 1] = rezerv;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.Write("{0}\n", numbers[i]);
+                }
+            }
+            if(test == "5")
+            {
+                //DateTime d;
+                //string data = Convert.d.ToString("22.12.2021");
+                //CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(d.DayOfWeek);
             }
         }
     }
